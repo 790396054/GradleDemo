b2a93a6d2af4960ecfbadbba5c1d5e322b3db2a7
gongmiaomiao <gongmiaomiao@didiglobal.com>
2022-11-19 13:46:59 +0800

    增加插件module， 增加路由注解处理器 module

--- app/src/main/java/com/didi/gradledemo/MainActivity.kt
+++ app/src/main/java/com/didi/gradledemo/MainActivity.kt
@@ -2,7 +2,9 @@ package com.didi.gradledemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.imooc.router.annotations.Destination
 
+@Destination(url = "/home/page", description = "应用主页")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
